f33141401b02b4b42087f354eeb8305762433300
tianjl <tianjingle@sogou-inc.com>
2021-09-02 10:17:10 +0800

    init

--- src/sMain.py
+++ src/sMain.py
@@ -10,21 +10,60 @@ import os
 class sMain:
 
     def train(self):
-        print("1.个股参数确定")
+        print("1.个股参数计算 ")
         config = Config()
         for stock in config.myStock:
             if int(stock[4])<1 or int(stock[5])<4:
                 core=Core()
                 core.start=-1
-                scale = 30  # 文本进度条宽度
+                scale = 24  # 文本进度条宽度
                 a = np.arange(3, scale, 1)
                 tempProfit=-20
+                trainTemp=[]
                 for item in a:
-                    t=item+1
+                    temp=[]
+                    temp.append(item)
+                    temp.append(2 * item + 3)
+                    trainTemp.append(temp)
+
+                    temp=[]
+                    temp.append(item)
+                    temp.append(2 * item + 2)
+                    trainTemp.append(temp)
+
+                    temp=[]
+                    temp.append(item)
+                    temp.append(2 * item + 1)
+                    trainTemp.append(temp)
+
+                    temp=[]
+                    temp.append(item)
+                    temp.append(2 * item)
+                    trainTemp.append(temp)
+
+                    temp=[]
+                    temp.append(item)
+                    temp.append(2 * item-1)
+                    trainTemp.append(temp)
+
+                    temp=[]
+                    temp.append(item)
+                    temp.append(2 * item-2)
+                    trainTemp.append(temp)
+
+                    temp=[]
+                    temp.append(item)
+                    temp.append(2 * item-3)
+                    trainTemp.append(temp)
+
+                scale=len(trainTemp)
+                for k in range(len(trainTemp)):
+                    item=trainTemp[k][0]
+                    t=k+1
                     a = '*' * t  # 字符串被复制的次数，"*"表示百分比所表达的信息
                     b = '.' * (scale - t)
                     c = (t / scale) * 100  # 输出对应进度条的百分比
-                    z = 2 * item + 3
+                    z = trainTemp[k][1]
                     NewtonBuySall, profit, currentIndex = core.execute(stock[0], item, z,1)
                     ##print(str(item) + "\t" + str(z) + "\t" + str(profit))
                     if profit>tempProfit:
@@ -33,26 +72,22 @@ class sMain:
                         stock[5] = z
                         config.newTrainValue()
                     print("\r{:^3.0f}%[{}->{}]{:^3.0f},{:^3.0f},{:^3.3f}".format(c, a, b,item,z,profit), end="",flush=True)  # dur用来记录打印文本进度条所消耗的时间
-                print()
+        # print("\033[1;32;40m 1.配置文件规整  \033[0m")
 
 
 if __name__ == '__main__':
-    currentPath = os.getcwd()
-    print(currentPath)
-    if not os.path.exists(currentPath + "\\temp\\"):
-       os.makedirs(currentPath+"\\temp\\")
+    cur_path = os.path.abspath(os.path.dirname(__file__))
+    tempDir=cur_path+"/temp/"
+    if os.path.exists(tempDir)==False:
+        os.makedirs(tempDir)
     banner=Banner()
     banner.bannerShow()
     smain=sMain()
     smain.train()
     core=Core()
     config=Config()
-    # print("\033[1;32;40m 2.股票波段计算  \033[0m")
     print("2.股票波段计算")
-    if len(config.myStock)>=1:
-        core.start(config.myStock)
-    else:
-        print("没有待选股票~~~~~~")
+    core.start(config.myStock)
 
 
 
